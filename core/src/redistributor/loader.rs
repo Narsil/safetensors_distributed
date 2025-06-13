a9307f7da913590ab59d02d7411450dc829f0816
Nicolas Patry <patry.nicolas@protonmail.com>
2025-06-13 10:30:55 +0200

    Dead file.

--- core/src/redistributor/loader.rs
+++
@@ -1,9 +0,0 @@
-use super::{RedistributorError, Result, SafetensorsIndex};
-use crate::topology::{SharedInfo, Tensor, Topology};
-use log::trace;
-use reqwest::{Client, header::HeaderMap};
-use safetensors::tensor::Metadata;
-use std::collections::{BTreeMap, HashMap};
-use url::Url;
-
-impl super::core::Redistributor {}
